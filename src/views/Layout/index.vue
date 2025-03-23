eec26816c330d2e37af54647f0c267781df9cf90
1022394845 <1022394845@qq.com>
2025-03-23 14:36:56 +0800

    静态样式

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,17 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
 <template>
   <div>
-    Layout
+    <!-- 导航 -->
+    <LayoutNav></LayoutNav>
+    <!-- 头部 -->
+    <LayoutHeader></LayoutHeader>
+    <!-- 路由主体 -->
     <router-view></router-view>
+    <!-- 底部 -->
+    <LayoutFooter></LayoutFooter>
   </div>
 </template>
