05d17782067cd0a2a05488a2d9fe5441dd2a40f7
1022394845 <1022394845@qq.com>
2025-03-24 18:16:15 +0800

    基础架构

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,17 @@
-<template>
-  <div>Home</div>
-</template>
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNews from './components/HomeNews.vue'
+import HomeHots from './components/HomeHots.vue'
+import HomeProduct from './components/HomeProduct.vue'
+</script>
 
-<style scoped>
-div {
-  min-height: 100vh;
-}
-</style>
+<template>
+  <div class="container">
+    <HomeCategory></HomeCategory>
+    <HomeBanner></HomeBanner>
+  </div>
+  <HomeNews></HomeNews>
+  <HomeHots></HomeHots>
+  <HomeProduct></HomeProduct>
+</template>
