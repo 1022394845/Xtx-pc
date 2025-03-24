562d539b9199f6310b8fa43694f83eaabe0aa888
1022394845 <1022394845@qq.com>
2025-03-24 22:48:18 +0800

    hots

--- src/apis/layout.js
+++ src/apis/layout.js
@@ -8,3 +8,6 @@ export const getBannerListAPI = () => request.get('/home/banner')
 
 // 获取新鲜好物列表
 export const getNewsListAPI = () => request.get('/home/new')
+
+// 获取人气推荐列表
+export const getHotsListAPI = () => request.get('/home/hot')
--- src/views/Home/components/HomeNews.vue
+++ src/views/Home/components/HomeNews.vue
@@ -13,7 +13,7 @@ getNewsList()
 </script>
 
 <template>
-  <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+  <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
     <ul class="goods-list">
       <li v-for="item in newsList" :key="item.id">
         <RouterLink to="/">
