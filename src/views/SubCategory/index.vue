bcc7ea4dd836f4d9bbde7436a8c81c1825863f57
1022394845 <1022394845@qq.com>
2025-03-25 21:15:49 +0800

    加载更多

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -32,6 +32,18 @@ const tabChange = () => {
   reqData.value.page = 1
   getGoodsList()
 }
+
+// 加载更多
+const disabled = ref(false)
+const load = async () => {
+  reqData.value.page++
+  const { result } = await getSubCategoryAPI(reqData.value)
+  goodsList.value = [...goodsList.value, ...result.items]
+  // 全部加载完毕 停止监听
+  if (!result.items.length) {
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -53,7 +65,11 @@ const tabChange = () => {
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div
+        class="body"
+        v-infinite-scroll="load"
+        :infinite-scroll-disabled="disabled"
+      >
         <!-- 商品列表-->
         <GoodsItem
           v-for="goods in goodsList"
