01ce1bbadaa4fe34bf01594b579ce0cdba243249
1022394845 <1022394845@qq.com>
2025-03-26 21:54:23 +0800

    数据渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -8,6 +8,7 @@ const detail = ref({})
 const getDetail = async () => {
   const { result } = await getDetailAPI(route.params.id)
   detail.value = result
+  console.log(result)
 }
 onMounted(() => {
   getDetail()
@@ -16,13 +17,19 @@ onMounted(() => {
 
 <template>
   <div class="xtx-goods-page">
-    <div class="container">
+    <div class="container" v-if="detail.id">
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">母婴 </el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">跑步鞋 </el-breadcrumb-item>
-          <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
+          <el-breadcrumb-item
+            :to="{ path: `/category/${detail.categories[1].id}` }"
+            >{{ detail.categories[1].name }}
+          </el-breadcrumb-item>
+          <el-breadcrumb-item
+            :to="{ path: `/category/${detail.categories[0].id}` }"
+            >{{ detail.categories[0].name }}
+          </el-breadcrumb-item>
+          <el-breadcrumb-item>{{ detail.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 商品信息 -->
@@ -36,33 +43,33 @@ onMounted(() => {
               <ul class="goods-sales">
                 <li>
                   <p>销量人气</p>
-                  <p>100+</p>
+                  <p>{{ detail.salesCount }}</p>
                   <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                 </li>
                 <li>
                   <p>商品评价</p>
-                  <p>200+</p>
+                  <p>{{ detail.commentCount }}</p>
                   <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                 </li>
                 <li>
                   <p>收藏人气</p>
-                  <p>300+</p>
+                  <p>{{ detail.collectCount }}</p>
                   <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>400+</p>
+                  <p>{{ detail.brand.name }}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
             </div>
             <div class="spec">
               <!-- 商品信息区 -->
-              <p class="g-name">抓绒保暖，毛毛虫儿童鞋</p>
-              <p class="g-desc">好穿</p>
+              <p class="g-name">{{ detail.name }}</p>
+              <p class="g-desc">{{ detail.desc }}</p>
               <p class="g-price">
-                <span>200</span>
-                <span> 100</span>
+                <span>{{ detail.price }}</span>
+                <span>{{ detail.oldPrice }}</span>
               </p>
               <div class="g-service">
                 <dl>
@@ -99,12 +106,22 @@ onMounted(() => {
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in 3" :key="item.value">
-                      <span class="dt">白色</span>
-                      <span class="dd">纯棉</span>
+                    <li
+                      v-for="item in detail.details.properties"
+                      :key="item.name"
+                    >
+                      <span class="dt">{{ item.name }}</span>
+                      <span class="dd">{{ item.value }}</span>
                     </li>
                   </ul>
                   <!-- 图片 -->
+                  <el-image
+                    lazy
+                    v-for="img in detail.details.pictures"
+                    :key="img"
+                    :src="img"
+                    alt=""
+                  />
                 </div>
               </div>
             </div>
@@ -342,7 +359,8 @@ onMounted(() => {
     }
   }
 
-  > img {
+  .el-image {
+    display: block;
     width: 100%;
   }
 }
